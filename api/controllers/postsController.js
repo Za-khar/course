358ed8f8a3c84b6f2568f6710dc88f708e1cf2d6
Zakhar <london95vv@gmail.com>
2021-01-10 00:50:28 +0200

    Added database connection

--- api/controllers/postsController.js
+++ api/controllers/postsController.js
@@ -1,23 +1,34 @@
+const db = require('../services/db');
 
 class PostsController {
     async createPost(req, res) {
-        res.json('createPost');
+        const {title, content} = req.body;
+        const newPost = await db('posts').insert({title: title, content: content}).returning('*');
+        res.json(newPost);
     }
 
     async getPosts(req, res) {
-        res.json('getPosts');
+        const posts = await db('posts').select('*');
+        res.json(posts);
     }
 
     async getOnePost(req, res) {
-        res.json(`get post number ${req.params.id}`);
+        const id = req.params.id;
+        const post = await db('posts').select('*').where('id', id);
+        res.json(post);
     }
 
     async updatePost(req,res) {
-        res.json(`update post number ${req.params.id}`);
+        const id = req.params.id;
+        const {title, content} = req.body;
+        const post = await db('posts').where('id', id).update({title: title, content: content}).returning('*');
+        res.json(post);
     }
 
     async deletePost(req, res) {
-        res.json(`delete post number ${req.params.id}`);
+        const id = req.params.id;
+        const post = await db('posts').where('id', id).del(['id', 'title']);
+        res.json(post);
     }
 }
 
