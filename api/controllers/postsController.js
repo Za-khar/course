5c856030b25e513db16db7445ee3e92581c3f03a
Zakhar <london95vv@gmail.com>
2021-01-19 21:56:04 +0200

    Errors fix

--- api/controllers/postsController.js
+++ api/controllers/postsController.js
@@ -1,33 +1,35 @@
 const db = require('../services/db');
 
+const tableName = 'posts';
+
 class PostsController {
     async createPost(req, res) {
         const {title, content} = req.body;
-        const newPost = await db('posts').insert({title: title, content: content}).returning('*');
+        const newPost = await db(tableName).insert({title: title, content: content}).returning('*');
         res.json(newPost);
     }
 
     async getPosts(req, res) {
-        const posts = await db('posts').select('*');
+        const posts = await db(tableName).select('*');
         res.json(posts);
     }
 
     async getOnePost(req, res) {
         const id = req.params.id;
-        const post = await db('posts').select('*').where('id', id);
+        const post = await db(tableName).select('*').where('id', id);
         res.json(post);
     }
 
     async updatePost(req,res) {
         const id = req.params.id;
         const {title, content} = req.body;
-        const post = await db('posts').where('id', id).update({title: title, content: content}).returning('*');
+        const post = await db(tableName).where('id', id).update({title: title, content: content}).returning('*');
         res.json(post);
     }
 
     async deletePost(req, res) {
         const id = req.params.id;
-        const post = await db('posts').where('id', id).del(['id', 'title']);
+        const post = await db(tableName).where('id', id).del(['id', 'title']);
         res.json(post);
     }
 }
