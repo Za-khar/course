ee5e5769fb168002d52cfee35f4d7f478d3c7eaf
Zakhar <london95vv@gmail.com>
2021-02-06 14:56:02 +0200

    Deleted users route

--- front/src/App.js
+++ front/src/App.js
@@ -3,15 +3,12 @@ import './App.css';
 import UserPage from './components/UserPage/UserPage';
 import GuestPage from './containers/GuestPage';
 import {Route, Switch} from 'react-router-dom';
-import UsersRoute from './containers/UsersRoute/UsersRoute';
-
 
 function App() {
   return (
     <>
       <Switch>
         <Route path="/home" component={UserPage}/>
-        <Route component={UsersRoute}></Route>
         <Route path="/" component={GuestPage}/>  
       </Switch>
     </>
